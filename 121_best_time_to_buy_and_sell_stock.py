1048ab93bd616a5e7d8ba6d6b79bfd00ce574508
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-27 09:55:54 +0200

    Slightly faster

--- 121_best_time_to_buy_and_sell_stock.py
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -5,16 +5,16 @@ import pytest
 def maxProfit(prices: List[int]) -> int:
     profit = 0
 
-    maxLookup = {}
+    maxVal = prices[len(prices) - 1]
     for j in range(len(prices) - 1, 0, -1):
-        maxLookup[j] = max(maxLookup.get(j + 1, prices[j]), prices[j])
-        profit = max(profit, maxLookup[j] - prices[j - 1])
+        maxVal = max(maxVal, prices[j])
+        profit = max(profit, maxVal - prices[j - 1])
 
     return profit
 
 
 @pytest.mark.parametrize(
-    "arr, expected", [([7, 1, 5, 3, 6, 4], 5), ([7, 6, 4, 3, 1], 0)]
+    "arr, expected", [([7, 1, 5, 3, 6, 4], 5), ([7, 6, 4, 3, 1], 0), ([1, 2], 1)]
 )
 def test_maxProfit(arr: List[int], expected: int):
     assert maxProfit(arr) == expected
