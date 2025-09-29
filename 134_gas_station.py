27fd7f65649d2b6afa05187f29f141cc1b2bb1ad
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-29 10:47:08 +0200

    134 Better Solution

--- 134_gas_station.py
+++ 134_gas_station.py
@@ -1,48 +1,57 @@
 import pytest
 
 
-def canCompleteCircuitFromPosition(gas: list[int], cost: list[int], i: int) -> bool:
-    if gas[i] == 0:
-        return False
-
+def distanceFromPosition(gas: list[int], cost: list[int], i: int) -> int:
     gasTank = 0
     n = len(gas)
     travelled = 0
     while travelled < n:
         gasTank += gas[i] - cost[i]
         if gasTank < 0:
-            return False
+            return travelled
         i = (i + 1) % n
         travelled += 1
 
-    return gasTank >= 0
+    return travelled
 
 
 def canCompleteCircuit(gas: list[int], cost: list[int]) -> int:
-    n = len(gas)
-    for i in range(n):
-        if canCompleteCircuitFromPosition(gas, cost, i):
+    # Idea: if you can't travel from a to b, you can't get to b from any station between a and b
+    i = 0
+    while i < len(gas):
+        distance = distanceFromPosition(gas, cost, i)
+        if distance == len(gas):
             return i
+        i += max(distance, 1)
+
     return -1
 
 
 @pytest.mark.parametrize(
     "gas, cost, position, result",
     [
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 0, False),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 1, False),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 2, False),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3, True),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 4, False),
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 0, 0),
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 1, 0),
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 2, 0),
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3, 5),
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 4, 2),
+        ([2, 3, 4], [3, 4, 3], 0, 0),
+        ([2, 3, 4], [3, 4, 3], 1, 0),
+        ([2, 3, 4], [3, 4, 3], 2, 2),
     ],
 )
-def test_canCompleteCircuitFromPostion(gas, cost, position, result):
-    assert canCompleteCircuitFromPosition(gas, cost, position) == result
+def test_distanceFromPosition(gas, cost, position, result):
+    print(distanceFromPosition(gas, cost, position))
+    assert distanceFromPosition(gas, cost, position) == result
 
 
 @pytest.mark.parametrize(
     "gas, cost, output",
-    [([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3), ([2, 3, 4], [3, 4, 3], -1)],
+    [
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3),
+        ([2, 3, 4], [3, 4, 3], -1),
+        ([5, 8, 2, 8], [6, 5, 6, 6], 3),
+    ],
 )
 def test_canCompleteCircuit(gas, cost, output):
     assert canCompleteCircuit(gas, cost) == output
