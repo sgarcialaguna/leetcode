0a91a84a823b61d8f49298cc69ad14e3852269b0
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-29 11:19:32 +0200

    134 Simpler solution

--- 134_gas_station.py
+++ 134_gas_station.py
@@ -1,48 +1,22 @@
 import pytest
 
 
-def distanceFromPosition(gas: list[int], cost: list[int], i: int) -> int:
-    gasTank = 0
-    n = len(gas)
-    travelled = 0
-    while travelled < n:
-        gasTank += gas[i] - cost[i]
-        if gasTank < 0:
-            return travelled
-        i = (i + 1) % n
-        travelled += 1
-
-    return travelled
-
-
 def canCompleteCircuit(gas: list[int], cost: list[int]) -> int:
-    # Idea: if you can't travel from a to b, you can't get to b from any station between a and b
-    i = 0
-    while i < len(gas):
-        distance = distanceFromPosition(gas, cost, i)
-        if distance == len(gas):
-            return i
-        i += max(distance, 1)
+    # If total cost exceeds total gas, no need to check
+    if sum(cost) > sum(gas):
+        return -1
 
-    return -1
+    total = 0
+    start = 0
 
+    for i in range(len(gas)):
+        total += gas[i] - cost[i]
 
-@pytest.mark.parametrize(
-    "gas, cost, position, result",
-    [
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 0, 0),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 1, 0),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 2, 0),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3, 5),
-        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 4, 2),
-        ([2, 3, 4], [3, 4, 3], 0, 0),
-        ([2, 3, 4], [3, 4, 3], 1, 0),
-        ([2, 3, 4], [3, 4, 3], 2, 2),
-    ],
-)
-def test_distanceFromPosition(gas, cost, position, result):
-    print(distanceFromPosition(gas, cost, position))
-    assert distanceFromPosition(gas, cost, position) == result
+        if total < 0:
+            total = 0
+            start = i + 1
+
+    return start
 
 
 @pytest.mark.parametrize(
