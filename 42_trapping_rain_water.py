9de6a747b73048b03d39eea1d79d85a167f40862
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-29 15:30:23 +0200

    42 Handle special cases: strictly increasing and decreasing

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -12,7 +12,18 @@ def find_next_index(height: list[int], i: int) -> int:
     return max_index
 
 
+def is_strictly_increasing(l: list[int]):
+    return all(x < y for x, y in zip(l, l[1:]))
+
+
+def is_strictly_decreasing(l: list[int]):
+    return all(x > y for x, y in zip(l, l[1:]))
+
+
 def trap(height: list[int]) -> int:
+    if is_strictly_decreasing(height) or is_strictly_increasing(height):
+        return 0
+
     trapped = 0
     i = 0
 
@@ -35,6 +46,7 @@ def trap(height: list[int]) -> int:
         ([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
         ([4, 2, 0, 3, 2, 5], 9),
         ([4, 2, 3], 1),
+        (range(20000, 0, -1), 0),
     ],
 )
 def test_trap(height: list[int], expected: int):
