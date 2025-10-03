37d9c4a7bd3326921390ae13a774e98c3c9766dd
Santiago Garcia <santiago.garcia@posteo.de>
2025-10-03 16:38:52 +0200

    11 3Sum with binary search. Still too slow :-(

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,16 +1,51 @@
 import pytest
-import itertools
+
+
+def binary_search(haystack: list[int], needle: int) -> int:
+    lo = 0
+    hi = len(haystack) - 1
+
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        val = haystack[mid]
+
+        if val == needle:
+            return mid
+
+        if val < needle:
+            lo += 1
+
+        if val > needle:
+            hi -= 1
+
+    return -1
 
 
 def threeSum(nums: list[int]) -> list[list[int]]:
     result = []
-    for i, j, k in itertools.combinations(range(len(nums)), 3):
-        if nums[i] + nums[j] + nums[k] == 0:
-            triplet = sorted([nums[i], nums[j], nums[k]])
-            if triplet not in result:
-                result.append(triplet)
+    nums = sorted(nums)
+    for i in range(len(nums) - 2):
+        for j in range(i + 1, len(nums) - 1):
+            needle = -(nums[i] + nums[j])
+            k = binary_search(nums[j + 1 :], needle)
+            if k != -1:
+                triplet = [nums[i], nums[j], nums[k + j + 1]]
+                if triplet not in result:
+                    result.append([nums[i], nums[j], nums[k + j + 1]])
 
     return result
+
+
+@pytest.mark.parametrize(
+    "haystack, needle, expected",
+    [
+        ([-4, -1, -1, 0, 1, 2], -4, 0),
+        ([-4, -1, -1, 0, 1, 2], -0, 3),
+        ([-4, -1, -1, 0, 1, 2], -5, -1),
+    ],
+)
+def test_binarySearch(haystack: list[int], needle: int, expected: int):
+    assert binary_search(haystack, needle) == expected
 
 
 @pytest.mark.parametrize(
