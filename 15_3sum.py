9a62ff40346ee1cfcd9bb9eef3846a4bc4f925eb
Santiago Garcia <santiago.garcia@posteo.de>
2025-10-03 17:43:52 +0200

    15 3Sum with two pointers

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,51 +1,55 @@
 import pytest
 
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+# such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-def binary_search(haystack: list[int], needle: int) -> int:
-    lo = 0
-    hi = len(haystack) - 1
-
-    while lo <= hi:
-        mid = (lo + hi) // 2
-        val = haystack[mid]
-
-        if val == needle:
-            return mid
-
-        if val < needle:
-            lo += 1
-
-        if val > needle:
-            hi -= 1
-
-    return -1
+# Notice that the solution set must not contain duplicate triplets.
 
 
 def threeSum(nums: list[int]) -> list[list[int]]:
     result = []
     nums = sorted(nums)
     for i in range(len(nums) - 2):
-        for j in range(i + 1, len(nums) - 1):
-            needle = -(nums[i] + nums[j])
-            k = binary_search(nums[j + 1 :], needle)
-            if k != -1:
-                triplet = [nums[i], nums[j], nums[k + j + 1]]
-                if triplet not in result:
-                    result.append([nums[i], nums[j], nums[k + j + 1]])
+        # Do not reuse the same numbers
+        if i > 0 and nums[i - 1] == nums[i]:
+            continue
+
+        lo = i + 1
+        hi = len(nums) - 1
+        val = nums[i]
+
+        target = -val
+        while lo < hi:
+            if nums[lo] + nums[hi] > target:
+                hi -= 1
+            elif nums[lo] + nums[hi] < target:
+                lo += 1
+            else:
+                result.append([val, nums[lo], nums[hi]])
+
+                # Do not reuse the same numbers
+                oldLo = nums[lo]
+                oldHi = nums[hi]
+
+                while lo < hi and nums[lo] == oldLo:
+                    lo += 1
+
+                while lo < hi and nums[hi] == oldHi:
+                    hi -= 1
 
     return result
 
 
-@pytest.mark.parametrize(
-    "haystack, needle, expected",
-    [
-        ([-4, -1, -1, 0, 1, 2], -4, 0),
-        ([-4, -1, -1, 0, 1, 2], -0, 3),
-        ([-4, -1, -1, 0, 1, 2], -5, -1),
-    ],
-)
-def test_binarySearch(haystack: list[int], needle: int, expected: int):
-    assert binary_search(haystack, needle) == expected
+# @pytest.mark.parametrize(
+#     "haystack, needle, expected",
+#     [
+#         ([-4, -1, -1, 0, 1, 2], -4, 0),
+#         ([-4, -1, -1, 0, 1, 2], -0, 3),
+#         ([-4, -1, -1, 0, 1, 2], -5, -1),
+#     ],
+# )
+# def test_binarySearch(haystack: list[int], needle: int, expected: int):
+#     assert binary_search(haystack, needle) == expected
 
 
 @pytest.mark.parametrize(
