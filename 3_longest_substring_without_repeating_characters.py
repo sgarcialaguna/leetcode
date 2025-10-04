a4f212b0306e1df44e8a4f95d8b7d68e07a13769
Santiago Garcia <santiago.garcia@posteo.de>
2025-10-04 11:44:26 +0200

    3 Simpler solution, keep the characters in a set

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -2,30 +2,18 @@ import pytest
 
 
 def lengthOfLongestSubstring(s: str) -> int:
+    charSet = set()
     start = 0
-    end = 0
-    result = s[start : end + 1]
+    result = 0
 
-    while end < len(s):
-        result = s[start : end + 1]
-
-        # We already have duplicates
-        if len(set(result)) != len(result):
+    for end in range(len(s)):
+        while s[end] in charSet:
+            charSet.remove(s[start])
             start += 1
-            end += 1
-            continue
+        charSet.add(s[end])
+        result = max(result, end - start + 1)
 
-        if end + 1 < len(s) and s[end + 1] not in result:
-            # We can increase the substring's length
-            end += 1
-            result = s[start : end + 1]
-            continue
-
-        # We can't, slide one over
-        start += 1
-        end += 1
-
-    return len(result)
+    return result
 
 
 @pytest.mark.parametrize(
