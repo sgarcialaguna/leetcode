f06e9d72b3d50c3be5fcb8c1b6e6f9d511b45ab4
Santiago Garcia <santiago.garcia@posteo.de>
2025-10-09 10:24:02 +0200

    219 Contains Duplicate II Unoptimized solution with hashtable

---
+++ 219_contains_duplicate_ii.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+import pytest
+
+
+def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
+    """Given an integer array nums and an integer k, return true if there are two distinct indices i and j
+    in the array such that nums[i] == nums[j] and abs(i - j) <= k."""
+
+    lookup = defaultdict(list[int])
+
+    for idx, num in enumerate(nums):
+        arr = lookup[num]
+        arr.append(idx)
+        if len(arr) >= 2:
+            if arr[len(arr) - 1] - arr[len(arr) - 2] <= k:
+                return True
+
+    return False
+
+
+@pytest.mark.parametrize(
+    "nums, k, expected",
+    [
+        ([], 10, False),
+        ([1], 1, False),
+        ([1, 2, 3, 1], 3, True),
+        ([1, 0, 1, 1], 1, True),
+        ([1, 2, 3, 1, 2, 3], 2, False),
+    ],
+)
+def test_containsNearbyDuplicate(nums, k, expected):
+    assert containsNearbyDuplicate(nums, k) is expected
