2e66a2a64ca83653a40c0be1351f08fd6d5ac585
Santiago Garcia <santiago.garcia@posteo.de>
2025-10-09 09:42:48 +0200

    202 Happy Number simple solution

---
+++ 202_happy_number.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+def isHappy(n: int) -> bool:
+    """Write an algorithm to determine if a number n is happy.
+
+    A happy number is a number defined by the following process:
+
+    Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+    Those numbers for which this process ends in 1 are happy.
+
+    Return true if n is a happy number, and false if not."""
+
+    seenNumbers = set()
+    curr = n
+    while True:
+        nextNumber = 0
+        while curr > 0:
+            nextNumber += pow(curr % 10, 2)
+            curr = curr // 10
+        if nextNumber == 1:
+            return True
+        if nextNumber in seenNumbers:
+            return False
+        seenNumbers.add(nextNumber)
+        curr = nextNumber
+
+
+@pytest.mark.parametrize("n, expected", [(19, True), (2, False)])
+def test_isHappy(n, expected):
+    assert isHappy(n) is expected
